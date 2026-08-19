5236b6fb9abfb46f09c35263b9ecc5546342a74c
digambar <dbhujbal287@gmail.com>
2026-08-19 22:37:52 +0530

    linked list node created

--- 04-linked-list/Linkedlist.java
+++ 04-linked-list/Linkedlist.java
@@ -1,4 +1,18 @@
 class Linkedlist{
     // linked list are a dynamic in the nature 
-    
+      
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+
+    Linkedlist()
 }
