3df018900fe7ea8f279a319f18fb322bdc090d00
digambar <dbhujbal287@gmail.com>
2026-08-20 23:08:54 +0530

    feat:linked list class improved

--- 04-linked-list/Linkedlist.java
+++ 04-linked-list/Linkedlist.java
@@ -1,49 +1,98 @@
 class Linkedlist {
-    // linked list are a dynamic in the nature
 
-    private Node head;
+    private Node head; // 
     private Node tail;
     private int length;
 
-    class Node { // node class that create a each node .means a blueprint for a each node
+    class Node {
+
         int data;
         Node next;
 
-        Node(int data) { // node class constructor
+        Node(int data) {
             this.data = data;
         }
     }
 
-    Linkedlist(int data) { // linked list constructor which makes a actual linked list object
-        Node newnode = new Node(data); // Here actual node is created
+    Linkedlist(int data) { // constructor of the linked list 
+
+        Node newnode = new Node(data); // node calss object initlized here 
+
         head = newnode;
         tail = head;
         length = 1;
-
     }
 
-    public void printlist() { // prints the linked list .
+    public void printlist() { // prints the list 
+
         Node temp = head;
-        while (temp != null) {
+
+        while (temp != null) { //O(n) time 
             System.out.print(temp.data + "->");
             temp = temp.next;
         }
+
+        System.out.println("null");
     }
 
-    public void getHead() { // print head data
+    public void getHead() { //O(1) time complexity 
         System.out.println("head is " + head.data);
     }
 
-    public void getTail() { 
-        System.out.println(tail.data);
+    public void getTail() { //O(1) time complexity
+        System.out.println("tail is " + tail.data);
     }
 
-    public void append(int data) {  // insert the new node at the end of this list .
+    public void append(int data) { 
+
         Node newNode = new Node(data);
-        Node temp = head;
-        while(temp.next!=null){
-            temp=temp.next;
+
+        if (length == 0) {// if list is enpty make newnode head 
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode; //set new node to tail 
         }
-        temp.next=newNode;
+
+        length++;
+    }
+
+    public void removeLast() {
+
+        if (length == 0) { //corner cases 
+            return;
+        }
+
+        if (length == 1) { // corner case where only one node remining to remove 
+            head = null;
+            tail = null;
+            length = 0;
+            return;
+        }
+
+        Node pre = head;// node before last node 
+        Node temp = head; 
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+
+        length--;
+    }
+
+
+    public Node removefrist(){
+        if(length==0){
+            return null;
+        }
+
+        Node next=head.next;
+        head=head.next;
+        next=head;
     }
 }
--- 04-linked-list/Main.java
+++ 04-linked-list/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+
         Linkedlist list = new Linkedlist(4);
 
+        list.append(10);
+        list.append(20);
+        list.append(30);
+
         list.printlist();
-        list.getHead();
-        list.append(5);
+
+        list.removeLast();
+        list.printlist();
+
+        list.removeLast();
+        list.printlist();
+
+        list.removeLast();
+        list.printlist();
+
+        list.removeLast(); // empty list — should not crash
         list.printlist();
     }
 }
