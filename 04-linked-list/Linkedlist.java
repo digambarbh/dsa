b282436fab9f10302dba85201b9248074e829694
digambar <dbhujbal287@gmail.com>
2026-08-20 11:07:47 +0530

    linked list  implemented

--- 04-linked-list/Linkedlist.java
+++ 04-linked-list/Linkedlist.java
@@ -1,18 +1,49 @@
-class Linkedlist{
-    // linked list are a dynamic in the nature 
-      
+class Linkedlist {
+    // linked list are a dynamic in the nature
+
     private Node head;
     private Node tail;
     private int length;
 
-    class Node{
+    class Node { // node class that create a each node .means a blueprint for a each node
         int data;
         Node next;
-        Node(int data){
-            this.data=data;
+
+        Node(int data) { // node class constructor
+            this.data = data;
         }
     }
 
+    Linkedlist(int data) { // linked list constructor which makes a actual linked list object
+        Node newnode = new Node(data); // Here actual node is created
+        head = newnode;
+        tail = head;
+        length = 1;
 
-    Linkedlist()
+    }
+
+    public void printlist() { // prints the linked list .
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() { // print head data
+        System.out.println("head is " + head.data);
+    }
+
+    public void getTail() { 
+        System.out.println(tail.data);
+    }
+
+    public void append(int data) {  // insert the new node at the end of this list .
+        Node newNode = new Node(data);
+        Node temp = head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+    }
 }
---
+++ 04-linked-list/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist(4);
+
+        list.printlist();
+        list.getHead();
+        list.append(5);
+        list.printlist();
+    }
+}
